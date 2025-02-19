d6caf1d81404cf5bac4e889237437b38163993bb
g4vr3 <gdfizan@gmail.com>
2025-02-19 12:06:01 +0100

    Add category description field to ticket_category model

--- models/helpdesk_ticket_category.py
+++ models/helpdesk_ticket_category.py
@@ -1,7 +1,8 @@
 from odoo import models, fields
 
-class HelpdeskTicketCategory(models.Model):
-    _name = 'helpdesk.ticket.category'
-    _description = 'Categoría del Ticket de Soporte'
+class HelpdeskCategory(models.Model):
+    _name = 'helpdesk.category'
+    _description = 'Categoría de Ticket de Soporte'
 
-    name = fields.Char(string='Nombre de la categoría', required=True)
+    name = fields.Char(string='Nombre de la Categoría', required=True) #nombre de la categoria
+    description = fields.Text(string='Descripción') #descripcion de la categoria
